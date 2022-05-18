c1436826c3dfb13bcd2dbf0b21793857d241c4e3
AndrewGennadevich <105662710+AndrewGennadevich@users.noreply.github.com>
2022-05-18 14:07:48 +0300

    Update HomeWorkNomber1.py

--- HomeWorkNomber1.py
+++ HomeWorkNomber1.py
@@ -3,8 +3,8 @@ color: str = "Black"
 music: str = "PopSmoke"
 film: str = "AnatomyOfLove"
 print(name, color, music, film)
-print('\033[3;30;41m name')
-print('\033[3;30;41m black')
+print('\033[3;30;41m Andrew')
+print('\033[3;30;41m Black')
 print('\033[3;30;41m PopSmoke')
 print('\033[3;30;41m AnatomyOfLove')
 
