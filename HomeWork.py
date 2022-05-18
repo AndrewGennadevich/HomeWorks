3c26f19016de3fd20fefccb9af7c646201197a8f
AndrewGennadevich <105662710+AndrewGennadevich@users.noreply.github.com>
2022-05-18 13:59:06 +0300

    Add files via upload

---
+++ HomeWork.py
@@ -0,0 +1,11 @@
+name: str = "Andrew"
+color: str = "Black"
+music: str = "PopSmoke"
+film: str = "AnatomyOfLove"
+print(name, color, music, film)
+print('\033[3;30;41m name')
+print('\033[3;30;41m black')
+print('\033[3;30;41m PopSmoke')
+print('\033[3;30;41m AnatomyOfLove')
+
+
