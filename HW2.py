282e8c9b99aad68f8b1db7cfae3b653b0dacb814
AndrewGennadevich <105662710+AndrewGennadevich@users.noreply.github.com>
2022-05-23 12:11:15 +0300

    Update HW2.py

--- HW2.py
+++ HW2.py
@@ -1,17 +1,17 @@
 names = ['Andrew', 'Lena', 'Olha', 'Karina']
-print(names[2], names[3], names[4])
+print(names[-3], names[-2], names[-1])
 # Version 1
 a = []
 for i in range(101, 169):
     if i % 2 == 1:
         a.append(i)
-        print(i)
+print(i)
 # Version 2
 b = []
 for i in range(1, 99):
     if i % 2 == 1:
         b.append(i)
-        print(b)
+print(b)
 # Version 3
 x = list(range(1001, 1090, 2))
 print(x)
