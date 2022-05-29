d079a025f4de21b903ec215ce13faa477db87ffd
AndrewGennadevich <105662710+AndrewGennadevich@users.noreply.github.com>
2022-05-29 22:39:29 +0300

    Add files via upload

---
+++ калькулятор + тортл/turtle HW3.2.py
@@ -0,0 +1,20 @@
+import turtle as window
+
+window.pensize(4)
+window.pencolor('red')
+
+window.forward(100)
+window.right(90)
+window.forward(100)
+window.right(90)
+window.forward(100)
+window.right(90)
+window.forward(100)
+window.up()
+window.forward(100)
+
+window.mainloop()
+
+
+
+
